a933d41dd97d6e6ec00fce0c43067cf53d1213fe
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-11 13:14:37 +0200

    Create Controller.java

---
+++ ProyectoMaven/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class Controller {
+ 
+    @GetMapping
+    public String saludos(@RequestParam(required = false, defaultValue = "") String nombre) {
+        return "".equals(nombre) ? "Hola Maven!" : "Maven " + nombre;
+    }
+}
